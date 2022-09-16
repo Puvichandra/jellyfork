6aecc72b6c7e07690d1df0b70ad37b18dd588751
chandra <sekar_vishaal@yahoo.co.in>
2022-09-16 13:07:06 +0530

    burn poll2

--- pages/marketplace.js
+++ pages/marketplace.js
@@ -12,7 +12,7 @@ export default function marketplace() {
   return (
     <div className='h-screen '>
     <div className='flex flex-row justify-center  '>
-    <div className='basis-5/12  text-2xl w-full text-center  text-white py-56 md:py-96 xl:py-80 font-press' >Coming Soon...</div>
+    <div className='basis-5/12  text-2xl w-full text-center  text-white py-56 md:py-96 xl:py-80 ' >Coming Soon...</div>
     </div>
     
     
